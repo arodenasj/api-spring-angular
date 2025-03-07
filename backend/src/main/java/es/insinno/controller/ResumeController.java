2142fd6c72586435a2e97a3b415821b099dab357
AntonioRodenasJurado <antonio.rodenas@insinno.es>
2025-03-07 13:52:18 +0100

    refactor: clean up formatting in application.properties, pom.xml, and ResumeController.java

--- backend/src/main/java/es/insinno/controller/ResumeController.java
+++ backend/src/main/java/es/insinno/controller/ResumeController.java
@@ -1,26 +1,25 @@
 package es.insinno.controller;
 
-    import es.insinno.Resume;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.web.bind.annotation.*;
+import es.insinno.Resume;
+import org.springframework.web.bind.annotation.*;
 
-    import java.util.List;
+import java.util.List;
 
-    @RestController
-    @RequestMapping("/api/resumes")
-    @CrossOrigin("*")
-    public class ResumeController {
+@RestController
+@RequestMapping("/api/resumes")
+@CrossOrigin("*")
+public class ResumeController {
 
-        @GetMapping
-        public List<Resume> getResume() {
-            return List.of(
-                    new Resume(1L, "Antonio Ródenas", "anrodenasj@gmail.com", "123456789", "Calle Falsa 123", "Java Developer")
-            );
-        }
-
-        @GetMapping("/{id}")
-        public Resume getResumeById(@PathVariable Long id) {
-            return new Resume(1L, "Antonio Ródenas", "anrodenasj@gmail.com", "123456789", "Calle Falsa 123", "Java Developer");
-        }
-
+    @GetMapping
+    public List<Resume> getResume() {
+        return List.of(
+                new Resume(1L, "Antonio Ródenas", "anrodenasj@gmail.com", "123456789", "Calle Falsa 123", "Java Developer")
+        );
     }
+
+    @GetMapping("/{id}")
+    public Resume getResumeById(@PathVariable Long id) {
+        return new Resume(1L, "Antonio Ródenas", "anrodenasj@gmail.com", "123456789", "Calle Falsa 123", "Java Developer");
+    }
+
+}
