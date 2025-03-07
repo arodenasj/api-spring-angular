ed153fcfb4d401d4c8962af4cd9668d7adb3f17b
AntonioRodenasJurado <antonio.rodenas@insinno.es>
2025-03-07 14:10:44 +0100

    add routing for resumes and components in app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import {Routes} from '@angular/router';
+import {Resume} from './models/resume';
+import {ResumeListComponent} from './components/resume-list/resume-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'resumes', pathMatch: 'full'},
+  {path: 'resumes', component: Resume},
+  {path: 'resumes', component: ResumeListComponent},
+];
